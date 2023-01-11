8901e664b9a2cd59cd800d95bd478d4e428504fe
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-01-11 11:01:21 +0100

    Styled added

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    black: "#000",
+    silver: "#ccc",
+    alto: "#ddd",
+    teal: "#037272",
+    crimson: "#db143c",
+    blueLagoon: "#058585",
+    white: "#fff",
+    gallery: "#eee",
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+};
