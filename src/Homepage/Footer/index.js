cf0ff2e4c5da3293d582e00d876e052778bef5f3
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-03-17 11:58:02 +0100

    Footer text update

--- src/Homepage/Footer/index.js
+++ src/Homepage/Footer/index.js
@@ -18,9 +18,7 @@ const Footer = () =>
         <Email href={`mailto:${email}`}>{email}</Email>
 
         <Content>
-          I’m always open to new projects whenever I have the time. If you have
-          a website, dashboard or mobile app in mind and need some help to make
-          your ideas come to life, feel free to contact me.
+          I’m always open to new projects whenever I have the time. Feel free to contact me.
         </Content>
 
         <IconsContainer>
