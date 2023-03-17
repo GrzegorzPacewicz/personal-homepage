9524e358d5693654174c94f5e51790b927c9ef64
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-03-17 11:56:30 +0100

    Background added to Globalstyle.js

--- src/core/Globalstyle.js
+++ src/core/Globalstyle.js
@@ -15,5 +15,7 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         font-family: 'Inter', sans-serif;
+        background-color: ${({ theme }) => theme.elementColor.site.background};
+        transition: 0.5s;
     }
 `;
